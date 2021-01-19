44e816652f1f65a7225058ecbb40e4ab468b86f4
Jesper Mattiasson <57625574+Mattiassonjesper@users.noreply.github.com>
2021-01-19 12:16:17 +0100

    test

--- src/Main.js
+++ src/Main.js
@@ -14,7 +14,7 @@ class Main extends Component {
     return (
         <HashRouter>
         <div>
-          <h1>Simple SPA</h1>
+          <h1>Simple Single Page App</h1>
           <ul className="header">
           <li><NavLink to="/">Home</NavLink></li>
             <li><NavLink to="/newpost">New Post</NavLink></li>
